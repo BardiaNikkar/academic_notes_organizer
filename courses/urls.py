2dbdfbf6cbdf4e53f7ac2e8f7822612854eb118c
Bardia <bardiya2085@gmail.com>
2026-07-25 13:35:10 +0330

    create course_list page, now users can see their courses list after adding a course

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_course
+from .views import create_course, course_list
 
 urlpatterns = [
     path('create/', create_course, name='course_create'),
+    path('', course_list, name='course_list'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import CourseForm
+from .models import Course
 
 # Create your views here.
 
@@ -12,7 +13,12 @@ def create_course(request):
             course = form.save(commit=False)
             course.user = request.user
             course.save()
-            return redirect("dashboard")
+            return redirect("course_list")
     else:
         form = CourseForm()
     return render(request, 'courses/course_create.html', {'form':form})
+
+@login_required
+def course_list(request):
+    courses = Course.objects.filter(user=request.user)
+    return render(request, 'courses/course_list.html', {'courses':courses})
