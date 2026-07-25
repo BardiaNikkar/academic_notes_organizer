26b8b5e5039fd1a95029aa0c988f93e3c6a3c0e6
Bardia <bardiya2085@gmail.com>
2026-07-25 18:05:09 +0330

    finish working on courses model add login_required decorater to update_course view create delete option for our project now user can delete course

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_course, course_list, update_course
+from .views import create_course, course_list, update_course, delete_course
 
 urlpatterns = [
     path('create/', create_course, name='course_create'),
     path('', course_list, name='course_list'),
-    path('<int:pk>/update/', update_course, name='course_update')
+    path('<int:pk>/update/', update_course, name='course_update'),
+    path('<int:pk>/delete/', delete_course, name='course_delete'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -23,6 +23,7 @@ def course_list(request):
     courses = Course.objects.filter(user=request.user)
     return render(request, 'courses/course_list.html', {'courses':courses})
 
+@login_required
 def update_course(request, pk):
     course = get_object_or_404(Course, pk=pk, user=request.user)
     if request.method == 'POST':
@@ -33,3 +34,11 @@ def update_course(request, pk):
     else:
         form = CourseForm(instance=course)
     return render(request, 'courses/course_update.html', {'form':form, 'course':course})
+
+@login_required
+def delete_course(request, pk):
+    course = get_object_or_404(Course, pk=pk, user=request.user)
+    if request.method == 'POST':
+        course.delete()
+        return redirect('course_list')
+    return render(request, 'courses/course_delete.html', {'course':course})
